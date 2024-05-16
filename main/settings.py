34425b5b2387004f572828b3b9ce8697c0f3192e
ditosepha <osephashvili.dito@gmail.com>
2024-05-16 17:37:06 +0400

    add basic pagination

--- main/settings.py
+++ main/settings.py
@@ -124,7 +124,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 AUTH_USER_MODEL = 'app.CustomUser'
